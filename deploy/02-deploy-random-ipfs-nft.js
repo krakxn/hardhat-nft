6be4482c6fa10e94103d320da889c2519d423586
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 14:41:23 +0530

    Update 02-deploy-random-ipfs-nft.js

--- deploy/02-deploy-random-ipfs-nft.js
+++ deploy/02-deploy-random-ipfs-nft.js
@@ -5,11 +5,13 @@ const { storeImages, storeTokeUriMetadata } = require("../utils/uploadToPinata")
 
 const FUND_AMOUNT = "1000000000000000000000"
 const imagesLocation = "./images/randomNft/"
+
 let tokenUris = [
     "ipfs://QmaVkBn2tKmjbhphU7eyztbvSQU5EXDdqRyXZtRhSGgJGo",
     "ipfs://QmYQC5aGZu2PTH8XzbJrbDnvhj3gVs7ya33H9mqUNvST3d",
     "ipfs://QmZYmH5iDbD6v3U2ixoVAjioSzvWJszDzYdbeCLquGSpVm",
 ]
+
 // const imageUris = [
 //     "ipfs://QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8",
 //     "ipfs://QmYx6GsYAKnNzZ9A6NvEKV9nf1VaDzJrqDR23Y8YSkebLU",
@@ -39,12 +41,13 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     }
 
     if (chainId == 31337) {
-        // create VRFV2 Subscription
+        // Create VRFV2 Subscription
         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
         const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
         const transactionReceipt = await transactionResponse.wait()
         subscriptionId = transactionReceipt.events[0].args.subId
+        
         // Fund the subscription
         // Our mock makes it so we don't actually have to worry about sending fund
         await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
@@ -54,7 +57,8 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     }
 
     log("----------------------------------------------------")
-    arguments = [
+    
+    const arguments = [
         vrfCoordinatorV2Address,
         subscriptionId,
         networkConfig[chainId]["gasLane"],
@@ -62,6 +66,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         networkConfig[chainId]["callbackGasLimit"],
         tokenUris,
     ]
+    
     const randomIpfsNft = await deploy("RandomIpfsNft", {
         from: deployer,
         args: arguments,
@@ -77,11 +82,11 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 }
 
 async function handleTokenUris() {
-    // Check out https://github.com/PatrickAlphaC/nft-mix for a pythonic version of uploading
-    // to the raw IPFS-daemon from https://docs.ipfs.io/how-to/command-line-quick-start/
+    // The raw IPFS-daemon from https://docs.ipfs.io/how-to/command-line-quick-start/
     // You could also look at pinata https://www.pinata.cloud/
     tokenUris = []
     const { responses: imageUploadResponses, files } = await storeImages(imagesLocation)
+    
     for (imageUploadResponseIndex in imageUploadResponses) {
         let tokenUriMetadata = { ...metadataTemplate }
         tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "")
@@ -91,6 +96,7 @@ async function handleTokenUris() {
         const metadataUploadResponse = await storeTokeUriMetadata(tokenUriMetadata)
         tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`)
     }
+    
     console.log("Token URIs uploaded! They are:")
     console.log(tokenUris)
     return tokenUris
