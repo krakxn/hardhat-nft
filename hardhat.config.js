bc0cc2dd1ab1a85a3cdb0a6b5d7bc6edef546a6e
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 14:51:51 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,12 +4,10 @@ require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 require("solidity-coverage");
 require("hardhat-deploy");
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 const RINKEBY_RPC_URL =
   process.env.RINKEBY_RPC_URL ||
@@ -53,8 +51,8 @@ module.exports = {
   },
   namedAccounts: {
     deployer: {
-      default: 0, // here this will by default take the first account as deployer
-      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      default: 0, // Here, this will by default take the first account as deployer
+      1: 0, // Similarly, on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
     },
   },
   mocha: {
