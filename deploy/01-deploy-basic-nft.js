c8325b88f42c8ebb9f815884588cb78a882753cc
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 14:39:25 +0530

    Update 01-deploy-basic-nft.js

--- deploy/01-deploy-basic-nft.js
+++ deploy/01-deploy-basic-nft.js
@@ -5,9 +5,10 @@ const { verify } = require("../utils/verify")
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const arguments = []
 
     log("----------------------------------------------------")
-    arguments = []
+    
     const basicNft = await deploy("BasicNft", {
         from: deployer,
         args: arguments,
