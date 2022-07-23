e0ccdd4b8c0c8b6ce168d7fcd9e619dcee9cd2d5
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 14:49:29 +0530

    Update uploadToNftStorage.js

--- utils/uploadToNftStorage.js
+++ utils/uploadToNftStorage.js
@@ -1,4 +1,5 @@
 // Import the NFTStorage class and File constructor from the 'nft.storage' package
+
 const { NFTStorage, File } = require("nft.storage")
 const mime = require("mime")
 const fs = require("fs")
@@ -17,19 +18,23 @@ async function storeNFTs(imagesPath) {
     const fullImagesPath = path.resolve(imagesPath)
     const files = fs.readdirSync(fullImagesPath)
     let responses = []
+    
     for (fileIndex in files) {
         const image = await fileFromPath(`${fullImagesPath}/${files[fileIndex]}`)
         const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
         const dogName = files[fileIndex].replace(".png", "")
+        
         const response = await nftstorage.store({
             image,
             name: dogName,
             description: `An adorable ${dogName}`,
-            // Currently doesn't support attributes 😔
+            // Currently, doesn't support attributes :(
             // attributes: [{ trait_type: "cuteness", value: 100 }],
         })
+        
         responses.push(response)
     }
+    
     return responses
 }
 
